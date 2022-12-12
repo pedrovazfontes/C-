2984eab63a8cb878309237c3d030e3ff001a5051
Pedro <pedrohenriquefontes9@gmail.com>
2022-12-12 00:29:54 -0300

    feat: Inicio Projeto Editor

---
+++ ProjetoHtml/EditorHtml/Menu.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show(){
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;//Cor da tela
+            Console.ForegroundColor = ConsoleColor.Blue;//Cor da fonte
+
+            
+
+        }
+    }
+}
