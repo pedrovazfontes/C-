eb68db45a1340ce2a5ed1c166aa32b822a366ea2
Pedro <pedrohenriquefontes9@gmail.com>
2022-12-12 00:48:30 -0300

    feat: Add layout da Tela

--- ProjetoHtml/EditorHtml/Menu.cs
+++ ProjetoHtml/EditorHtml/Menu.cs
@@ -6,11 +6,41 @@ namespace EditorHtml
     {
         public static void Show(){
             Console.Clear();
-            Console.BackgroundColor = ConsoleColor.Blue;//Cor da tela
+            Console.BackgroundColor = ConsoleColor.Black;//Cor da tela
             Console.ForegroundColor = ConsoleColor.Blue;//Cor da fonte
 
             
+            DrawScreen();
+        }
+
+        public static void DrawScreen(){
+            Cabecalho();
+            Linhas();
+            Cabecalho();
+    
+        }
+
+        public static void Cabecalho(){
+            Console.Write("+");
+            for (int i = 0; i<=30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
 
         }
+
+        public static void Linhas(){
+            for (int lines = 0;lines<10;lines++){
+                    Console.Write("|");
+                    for(int i =0;i<=30;i++)
+                        Console.Write(" ");
+                    
+                    Console.Write("|");
+                    Console.Write("\n");
+                }
+        }
+        
     }
+    
 }
